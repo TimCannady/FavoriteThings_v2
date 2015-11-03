e118604ad40d4bebaf6a6cc9aea8b446c86284f1
Tim Cannady <timcannady@gmail.com>
2015-11-03 15:45:41 -0800

    add framework for user_show. Loading data

--- app/assets/javascripts/components/Items/Item_Show.js.jsx
+++ app/assets/javascripts/components/Items/Item_Show.js.jsx
@@ -3,8 +3,30 @@ var ItemShow = React.createClass({
 		return {
 			didFetchData: false,
 			headerImage: "http://www.w3schools.com/html/html5.gif",
-			items: []
+			itemName: "",
+			itemDescription: "",
+			itemPhotoUrl: ""
 		}
+	},
+
+	componentDidMount: function(){
+		this.fetchData()
+	},
+
+	fetchData: function(){
+		var params = this.props.params.id
+		$.ajax({
+			type: "GET",
+			url: "/items/" + params,
+			data: "data",
+			dataType: 'json',
+			success: function(data){
+				this.setState({didFetchData: 'true', itemName: data.name, itemDescription: data.description, itemPhotoUrl: data.photo_url, headerImage: data.photo_url})
+			}.bind(this),
+			error: function(data){
+				alert('error!')
+			}
+		})
 	},
 	
 	render: function(){
@@ -12,7 +34,11 @@ var ItemShow = React.createClass({
 			<div>
 				<Header img_src={this.state.headerImage} />
 				<br />
-				item show
+				<img src={this.state.itemPhotoUrl} />
+				<br />
+				Name: {this.state.itemName} 
+				<br />
+				Description: {this.state.itemDescription} 
 			</div>
 		)
 	}
