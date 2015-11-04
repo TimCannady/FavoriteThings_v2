0d83cf833b4cad9368e17ea6e936ccc5ea433a99
Tim Cannady <timcannady@gmail.com>
2015-11-03 19:59:33 -0800

    add css to items_show and placeholder (+) link for add button

--- app/assets/javascripts/components/Items/Item_Card.js.jsx
+++ app/assets/javascripts/components/Items/Item_Card.js.jsx
@@ -2,13 +2,16 @@ var ItemCard = React.createClass({
 	render: function(){
 		return(
 			<div className="card-wrapper">
-				<Link to="itemShow" params={{id: this.props.id}} > 
-					item name: {this.props.name}
+				<Link to="itemShow" params={{id: this.props.id}} className="card-text" > 
+					<div className="card-img-wrapper">
+						<img src={this.props.photo_url} className="card-img" />
+					</div>
+					{this.props.name}
+				</Link>	
+				<br/>
+				<Link to="itemShow" params={{id: this.props.id}} className="add-item-button" >
+					+
 				</Link>
-				<br/>
-				id: {this.props.id}
-				<br/>
-				<img src={this.props.photo_url} className="card-img" />
 			</div>
 		)
 	}
