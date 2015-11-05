a07c5212bfa57772cafb3eb6dd22dd24d953c2d4
Tim Cannady <timcannady@gmail.com>
2015-11-05 14:14:05 -0800

    fix alert but

--- app/assets/javascripts/components/Items/Item_Card.js.jsx
+++ app/assets/javascripts/components/Items/Item_Card.js.jsx
@@ -1,4 +1,65 @@
 var ItemCard = React.createClass({
+
+	getInitialState: function(){
+		return{itemID: this.props.id}
+	},
+
+	componentDidMount: function(){
+		newState = this.currentUserID()
+		this.setState({userID: newState})
+	},
+
+	currentUserID: function(){
+		if(App.checkLoggedIn()){
+			var email = this.currentUserEmail()
+			this.fetchUserID(email)
+		}else{
+		}
+	},
+
+	currentUserEmail: function(){
+		return localStorage.getItem('email')
+	},
+
+	fetchUserID: function(email){
+		$.ajax({
+			type: "GET",
+			url: "/users/email",
+			data: {email: email},
+			dataType: 'json',
+			success: function(data){
+				this.setState({didFetchData: 'true', userID: data.user_id})
+			}.bind(this),
+			error: function(data){
+				// alert('error! couldnt fetch user id')
+			}
+		})
+	},
+
+	addLike: function(){
+		var data = {
+		   itemID: this.state.id,
+		}
+		 // Submit form via jQuery/AJAX
+		$.ajax({
+			type: 'POST',
+			url: '/items/' + this.state.id + '/like',
+			data: data
+			})
+			.done(function(data) {
+				//cange he color of the like button to something else
+				alert('like successful!')
+			}.bind(this))
+			.fail(function(data) {
+				alert('failed to like item')
+			});
+	},
+
+	handleLike: function(e){
+		e.preventDefault()
+		this.addLike()
+	},
+
 	render: function(){
 		return(
 			<div className="card-wrapper">
@@ -9,9 +70,7 @@ var ItemCard = React.createClass({
 					{this.props.name}
 				</Link>	
 				<br/>
-				<Link to="itemShow" params={{id: this.props.id}} className="add-item-button" >
-					+
-				</Link>
+				<a href="" onClick={this.handleLike}> Like </a>
 			</div>
 		)
 	}
