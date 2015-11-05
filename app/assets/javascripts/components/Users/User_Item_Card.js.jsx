806500cb25fa7fa3d6d74c86e7329cb63b013ca8
Tim Cannady <timcannady@gmail.com>
2015-11-05 14:41:22 -0800

    Add like functionality to user_show_Card

--- app/assets/javascripts/components/Users/User_Item_Card.js.jsx
+++ app/assets/javascripts/components/Users/User_Item_Card.js.jsx
@@ -1,4 +1,68 @@
 var UserItemCard = React.createClass({
+	getInitialState: function(){
+		return{
+			itemID: this.props.id,
+			userID: null,
+		}
+	},
+
+	componentDidMount: function(){
+		newState = this.currentUserID()
+		this.setState({userID: newState})
+	},
+
+	currentUserID: function(){
+		if(App.checkLoggedIn()){
+			var email = this.currentUserEmail()
+			this.fetchUserID(email)
+		}else{
+		}
+	},
+
+	currentUserEmail: function(){
+		return localStorage.getItem('email')
+	},
+
+	fetchUserID: function(email){
+		$.ajax({
+			type: "GET",
+			url: "/users/email",
+			data: {email: email},
+			dataType: 'json',
+			success: function(data){
+				this.setState({didFetchData: 'true', userID: data.user_id})
+			}.bind(this),
+			error: function(data){
+				alert('error! couldnt fetch user id')
+			}
+		})
+	},
+
+	addLike: function(){
+		var data = {
+		   itemID: this.state.itemID,
+		   userID: this.state.userID
+		}
+		 // Submit form via jQuery/AJAX
+		$.ajax({
+			type: 'POST',
+			url: '/items/' + this.state.userID + '/like',
+			data: data
+			})
+			.done(function(data) {
+				//cange he color of the like button to something else
+				alert('like successful!')
+			}.bind(this))
+			.fail(function(data) {
+				alert('failed to like item')
+			});
+	},
+
+	handleLike: function(e){
+		e.preventDefault()
+		this.addLike()
+	},
+	
 	render: function(){
 		return(
 			<div className="card-wrapper">
@@ -11,9 +75,7 @@ var UserItemCard = React.createClass({
 
 				<br/>
 
-				<Link to="itemShow" params={{id: this.props.id}} className="add-item-button" >
-					+
-				</Link>
+				<a href="" onClick={this.handleLike}> Like </a>
 
 			</div>
 		)
