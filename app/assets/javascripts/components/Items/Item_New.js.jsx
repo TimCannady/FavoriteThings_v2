9bf9305713a8e54a37952787cef58d4a20e4fff2
Tim Cannady <timcannady@gmail.com>
2015-11-08 19:10:58 -0800

    add functionality for name, desc and photo_url state to update on item_new

--- app/assets/javascripts/components/Items/Item_New.js.jsx
+++ app/assets/javascripts/components/Items/Item_New.js.jsx
@@ -1,8 +1,19 @@
 var ItemNew = React.createClass({
+
+	getInitialState: function(){
+		return {
+			userID: localStorage.getItem('userID'),
+			headerImage: "http://www.w3schools.com/html/html5.gif"
+		}
+	},
+
 	render: function(){
 		return(
 			<div>
-				item form
+				<Header img_src={this.state.headerImage} />
+				<section className="body-wrapper">
+					<NewItemForm userID={this.state.userID} />
+				</section>
 			</div>
 		)
 	}
---
+++ app/assets/javascripts/components/Items/NewItemForm.js.jsx
@@ -0,0 +1,66 @@
+var NewItemForm = React.createClass({
+
+	getInitialState: function(){
+		return{
+			name: "",
+			description: "",
+			photoURL: "",
+		}
+	},
+
+	submit: function(e){
+		// var self 
+		e.preventDefault()
+		// self = this
+
+		var data = {
+		   name: this.state.name,
+		   description: this.state.description,
+		   photo_url: this.state.photoURL,
+		}
+
+		 // Submit form via jQuery/AJAX
+		$.ajax({
+			type: 'POST',
+			url: '/items',
+			data: data
+			})
+			.done(function(data) {
+				App.logIn(data.email)
+				alert('item creation successful!')
+				// self.clearForm()
+			}.bind(this))
+			.fail(function(data) {
+				alert('Failed to create item')
+			});
+	},
+
+	handleNameChange: function(event) {
+	    this.setState({name: event.target.value});
+	},
+
+	handleDescriptionChange: function(event) {
+	    this.setState({description: event.target.value});
+	}, 
+	handlePhotoURLChange: function(event) {
+	    this.setState({photoURL: event.target.value});
+	},
+
+	render: function(){
+		return(
+			<div className="login-signup-form-wrapper">
+				Create New Item:
+				<br/>
+				<form onSubmit={this.submit} className="login-signup-form" className="login-signup-form" >
+				 	Name: <input label="Name:" type="text" onChange={this.handleNameChange} />
+				 	<br/>
+				 	Description: <input label="Description:" type="text" onChange={this.handleDescriptionChange} />
+				 	<br/>
+				 	Photo URL: <input label="photoURL:" type="text" onChange={this.handlePhotoURLChange} />
+				 	<br/>
+				 	<button type="submit">Submit</button>
+				</form>
+			</div>
+		)
+	}
+})
