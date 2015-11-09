1f8e16ba6eebacab0e3ada25128921062dac07b9
Tim Cannady <timcannady@gmail.com>
2015-11-08 17:58:47 -0800

    update removeItemFromDom on the UnlikeButton so it only triggers if it exists - eg if it was passed down from the parent

--- app/assets/javascripts/components/Unlike_Button.js.jsx
+++ app/assets/javascripts/components/Unlike_Button.js.jsx
@@ -19,7 +19,9 @@ var UnlikeButton = React.createClass({
 			url: '/items/' + this.state.itemID + '/unlike',
 			data: data,
 			success: function(data){
-				this.props.removeItemFromDOM() // update the view if the user removes an item. Only does this from User_Item_Card because it's the only one that needs to appear to be live since you're on the page it removes it from.
+				if(this.props.removeItemFromDOM){ // only removeItemFromDOM() if the method has been passed down. In short, we only want/need to remove an item from the DOM if we're clicking the unlike button while on the logged-in user's show. 
+					this.props.removeItemFromDOM() // update the view if the user removes an item. Only does this from User_Item_Card because it's the only one that needs to appear to be live since you're on the page it removes it from.
+				} 
 			}.bind(this),
 			error: function(data){
 				alert('failed to unlike item!')
